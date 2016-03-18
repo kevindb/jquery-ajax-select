d7a1deed30b83d07ea9a38f15f1f9ac2af71a801
Kevin Morris <kmorri36@jhu.edu>
2016-03-18 15:27:44 -0400

    Updates license from GPL to LGPL

--- README.md
+++ README.md
@@ -2,6 +2,5 @@
 This is a JQuery plugin that populates an HTML Select element with options from an AJAX call.
 
 ## License
-
-This repository is licensed under the GNU General Public License v2.
+This repository is licensed under the GNU Lesser General Public License v2.1.  
 See [LICENSE](LICENSE) for details.
--- src/jquery.ajaxSelect.js
+++ src/jquery.ajaxSelect.js
@@ -1,3 +1,13 @@
+/*
+* Copyright 2015 Kevin Morris
+*
+* This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either version 2.1 of the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+*/
+
 (function($) {
 	// Plugin definition
 	$.fn.ajaxSelect = function(settings) {
